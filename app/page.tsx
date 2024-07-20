fece5de29ee2beba145ab387344a9d64ef39b3eb
ThomASS33 <mikequeer3883@gmail.com>
2024-07-20 08:58:04 -0400

    sofaKING sore <3

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24 ">
-  <div className="relative w-full h-80 flex items-center justify-center" style={{ marginTop: '-15%' }}>
+    <main className="flex flex-col items-center justify-between p-24 min-h-screen">
+  <div className="relative w-full h-80 flex items-center justify-center" style={{ marginTop: '-75%' }}>
 
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -20,63 +20,43 @@ export default function Home() {
   <div style={{ height: '10px' }}></div>
 
   <div className="mb-32 grid gap-4 justify-center text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-    <a
-      href="https://www.dextools.io/app/en/solana/pair-explorer/Gs6R82BqziwMW5HbEnkzToPmJfH2x6Ucy8gXFvvF6wwj?t=1721220326476"
-      className="group relative rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ padding: "8px" }}
-    >
-      <div className="relative z-10 transition-opacity duration-300 group-hover:opacity-0 custom-font">
-        <h2 className="mb-3 text-2xl font-semibold text-left font-custom">
-          $BrettGold{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-sm opacity-50 text-left font-custom">
-          Follow to view now on Dextools.io
-        </p>
-      </div>
-      {/* Image that displays when hovered */}
-      <img
-        src="/dextools-logo.svg"
-        alt="Dextools Logo"
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-0"
-        style={{ mixBlendMode: 'multiply' }}
-      />
-    </a>
+   
 
     <a
-      href="https://linktr.ee/brettgoldsol"
-      className="group relative rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{ padding: "8px" }}
-    >
-      <div className="relative z-10 transition-opacity duration-300 group-hover:opacity-0 custom-font">
-        <h2 className="mb-3 text-2xl font-semibold font-custom">
-          #BrettGold{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-sm opacity-50 font-custom">
-          Follow to Linktr.ee
-        </p>
-      </div>
-      {/* Image that displays when hovered */}
-      <img
-        src="/linktree-logo.svg"
+  href="https://www.dextools.io/app/en/solana/pair-explorer/Gs6R82BqziwMW5HbEnkzToPmJfH2x6Ucy8gXFvvF6wwj?t=1721220326476"
+  className="group relative z-10 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start w-32 h-32"
+  target="_blank"
+  rel="noopener noreferrer"
+  style={{ padding: "8px" }}
+>
+  {/* Image that displays on hover */}
+  <img
+        src="/dextools-logo.svg"
+        alt="Dextools Logo"
+    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 opacity-100 transition-opacity duration-300 group-hover:opacity-100 z-10"
+    style={{ mixBlendMode: 'multiply' }}
+  />
+</a>
+  
+    <a
+  href="https://linktr.ee/brettgoldsol"
+  className="group relative z-10 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start w-32 h-32"
+  target="_blank"
+  rel="noopener noreferrer"
+  style={{ padding: "8px" }}
+>
+  {/* Image that displays on hover */}
+  <img
+   src="/linktree-logo.svg"
         alt="Linktree Logo"
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-0"
-        style={{ mixBlendMode: 'multiply' }}
-      />
-    </a>
+     className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 opacity-100 transition-opacity duration-300 group-hover:opacity-100 z-10"
+    style={{ mixBlendMode: 'multiply' }}
+  />
+</a>
 
     <a
   href="https://x.com/GOLDENBRETTSOL"
-  className="group relative rounded-lg  px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start"
+  className="group relative z-10 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start w-32 h-32"
   target="_blank"
   rel="noopener noreferrer"
   style={{ padding: "8px" }}
@@ -85,7 +65,7 @@ export default function Home() {
   <img
     src="/twitter-logo.svg"
     alt="Twitter Logo"
-    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 opacity-100 transition-opacity duration-300 group-hover:opacity-100 z-10"
+    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-7/11 opacity-100 transition-opacity duration-300 group-hover:opacity-100 z-10"
     style={{ mixBlendMode: 'multiply' }}
   />
 </a>
@@ -93,30 +73,18 @@ export default function Home() {
 
 
 
-    <Link href="/sokoban-gold" passHref>
-  <div className="group relative z-10 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start" style={{ padding: "8px" }}>
+<Link href="/sokoban-gold" passHref>
+  <div className="group relative z-10 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-green-500 hover:bg-yellow-400 hover:dark:border-yellow-400 hover:dark:bg-green-500 flex items-start justify-start w-32 h-32">
     {/* Image that displays on load */}
     <img
       src="/pow-logo.svg"
       alt="Linktree Logo"
-      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 opacity-100 transition-opacity duration-300 z-0 group-hover:opacity-0"
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 opacity-100 transition-opacity duration-300 group-hover:opacity-100 z-10"
       style={{ mixBlendMode: 'multiply' }}
     />
-
-    {/* Text that appears on hover */}
-    <div className="relative custom-font opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-10">
-      <h2 className="mb-3 text-2xl font-semibold text-left">
-        Sokoban Gold{' '}
-        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          -&gt;
-        </span>
-      </h2>
-      <p className="m-0 max-w-[30ch] text-sm opacity-50 text-left">
-        PoW loading...
-      </p>
-    </div>
   </div>
 </Link>
+
 
 
 
